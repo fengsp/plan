4807fc6fc2c2de16f71812eec95d47f5a5d59827
Shipeng Feng <fsp261@gmail.com>
2014-06-19 12:19:15 +0800

    Head to 0.3-dev

--- plan/__init__.py
+++ plan/__init__.py
@@ -9,7 +9,7 @@
     :license: BSD, see LICENSE for more details.
 """
 
-__version__ = '0.2'
+__version__ = '0.3-dev'
 
 from .core import Plan
 from .job import Job, CommandJob, ScriptJob, ModuleJob, RawJob
