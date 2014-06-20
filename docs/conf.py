9170af63dab3788d35e3ed84f3ccd3474cff3302
Shipeng Feng <fsp261@gmail.com>
2014-06-20 11:26:25 +0800

    Do not use flask theme

--- docs/conf.py
+++ docs/conf.py
@@ -100,7 +100,7 @@ exclude_patterns = ['_build']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'flask'
+# html_theme = 'flask'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
