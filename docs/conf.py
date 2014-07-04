1b614ffd33f48fd228e94c525081e133c0e5b491
Shipeng Feng <fsp261@gmail.com>
2014-07-05 01:04:16 +0800

    Fixed no h1 in docs

--- docs/conf.py
+++ docs/conf.py
@@ -112,7 +112,9 @@ html_theme = 'flask'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'index_logo': ''
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ['_themes']
